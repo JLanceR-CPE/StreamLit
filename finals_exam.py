8cf1c7f3e7457805c82012f21eb0e6c72cd72048
JLanceR-CPE <133870730+JLanceR-CPE@users.noreply.github.com>
2023-05-17 23:38:45 +0800

    Update finals_exam.py

--- finals_exam.py
+++ finals_exam.py
@@ -31,7 +31,8 @@ warnings.filterwarnings("ignore")
 import os
 #from matplotlib.image import imread
 import random
-import matplotlib.image as mpimg
+#import 
+matplotlib.image as mpimg
 
 import pathlib
 dataset = "/content/drive/MyDrive/Colab Notebooks/RONQUILLO_Final Exam/images"
