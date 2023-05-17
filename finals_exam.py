43293f218751af3f738dd571169ea045c6e9f87b
JLanceR-CPE <133870730+JLanceR-CPE@users.noreply.github.com>
2023-05-17 23:12:48 +0800

    Update finals_exam.py

--- finals_exam.py
+++ finals_exam.py
@@ -18,7 +18,7 @@ Original file is located at
 
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
 import seaborn as sns
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.model_selection import train_test_split
